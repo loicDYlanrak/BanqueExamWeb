dbb0431a1264fc53d5691949061e71459857337c
Noms-001 <myahaintso@gmail.com>
2025-07-07 10:57:10 +0300

    base.sql

--- sql/table.sql
+++ sql/table.sql
@@ -3,7 +3,7 @@ CREATE DATABASE banque CHARACTER SET utf8mb4;
 USE banque;
 -- 1. Table des fonds de l'EF (pas besoin de table EF car unique)
 CREATE TABLE fonds (
-    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    id INT PRIMARY KEY AUTO_INCREMENT,
     montant DECIMAL(12, 2) NOT NULL,
     date_ajout DATETIME DEFAULT CURRENT_TIMESTAMP,
     description TEXT
@@ -11,15 +11,15 @@ CREATE TABLE fonds (
 
 -- 2. Table des types de prêts
 CREATE TABLE type_pret (
-    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    id INT PRIMARY KEY AUTO_INCREMENT,
     nom VARCHAR(50) NOT NULL ,
     taux DECIMAL(5, 2) NOT NULL ,
-    duree_mois INTEGER NULL
+    duree_mois INT NULL
 );
 
 -- 3. Table des clients
 CREATE TABLE client (
-    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    id INT PRIMARY KEY AUTO_INCREMENT,
     nom VARCHAR(50) NOT NULL,
     prenom VARCHAR(50) NOT NULL,
     telephone VARCHAR(20),
@@ -28,12 +28,12 @@ CREATE TABLE client (
 
 -- 4. Table des prêts (liés à un client et un type)
 CREATE TABLE pret (
-    id INTEGER PRIMARY KEY AUTOINCREMENT,
-    client_id INTEGER NOT NULL,
-    type_pret_id INTEGER NOT NULL,
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    client_id INT NOT NULL,
+    type_pret_id INT NOT NULL,
     montant DECIMAL(12, 2) NOT NULL,
     date_debut DATE NOT NULL,
-    duree_mois INTEGER NOT NULL,
+    duree_mois INT NOT NULL,
     est_actif BOOLEAN DEFAULT TRUE,
     FOREIGN KEY (client_id) REFERENCES client(id),
     FOREIGN KEY (type_pret_id) REFERENCES type_pret(id)
@@ -41,8 +41,8 @@ CREATE TABLE pret (
 
 -- 5. Table des remboursements
 CREATE TABLE remboursement (
-    id INTEGER PRIMARY KEY AUTOINCREMENT,
-    pret_id INTEGER NOT NULL,
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    pret_id INT NOT NULL,
     montant DECIMAL(10, 2) NOT NULL,
     date_remboursement DATETIME DEFAULT CURRENT_TIMESTAMP,
     FOREIGN KEY (pret_id) REFERENCES pret(id)
