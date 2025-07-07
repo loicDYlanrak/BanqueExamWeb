6b565d66cf7fa22a2ad4fc8c1b7065508f060615
Noms-001 <myahaintso@gmail.com>
2025-07-07 13:35:11 +0300

    Update Client

--- ws/index.php
+++ ws/index.php
@@ -21,6 +21,7 @@
     Flight::route('GET /clients', ['ClientController', 'getAll']);
     Flight::route('GET /clients/@id', ['ClientController', 'getById']);
     Flight::route('POST /clients', ['ClientController', 'create']);
+    Flight::route('PUT /clients/@id', ['ClientController','update']);
 
     Flight::start();
 ?>
